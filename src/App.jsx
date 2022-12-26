42f61b444f588170c35505cabd435c44ca2a0d78
aladdinalizada <tu7c5zneb@code.edu.az>
2022-12-27 01:33:31 +0400

    added files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
