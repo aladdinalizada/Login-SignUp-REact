d391be7ec9544539234f920d865308dbce6c7e86
aladdinalizada <tu7c5zneb@code.edu.az>
2022-12-28 00:09:47 +0400

    complete login page

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,7 +1,68 @@
 import React from "react";
-
+import "./login.css";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { Button, TextField } from "@mui/material";
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const validationSchema = yup.object({
+    email: yup
+      .string("Enter your Username")
+      .email("Username a valid email")
+      .required("Username is required"),
+    password: yup
+      .string("Enter your password")
+      .min(8, "Password should be of minimum 8 characters length")
+      .required("Password is required"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  const handleLogin = () => {
+    console.log("Login", formik.values);
+  };
+  return (
+    <div className="loginPage">
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="username"
+          name="username"
+          label="Username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          error={formik.touched.username && Boolean(formik.errors.username)}
+          helperText={formik.touched.username && formik.errors.username}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button
+          color="primary"
+          variant="contained"
+          fullWidth
+          type="submit"
+          onClick={() => handleLogin()}
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
---
+++ src/components/login/schema/index.js
@@ -0,0 +1,8 @@
+import * as Yup from "yup";
+
+export const loginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
